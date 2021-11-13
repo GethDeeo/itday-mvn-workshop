2550c305a234ac032a4f84b5129e22f4443d924b
GethDeeo <21061348+GethDeeo@users.noreply.github.com>
2021-11-13 12:18:46 +0100

    enabled Log4j for logging
    WildFly implicitly provides logging.

--- workshop-ejb/src/main/java/hu/tigra/itday/workshop/greeting/GreetingBean.java
+++ workshop-ejb/src/main/java/hu/tigra/itday/workshop/greeting/GreetingBean.java
@@ -3,12 +3,16 @@ package hu.tigra.itday.workshop.greeting;
 import javax.ejb.Stateless;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Stateless
 public class GreetingBean {
+  protected static final Logger LOGGER = LogManager.getLogger(GreetingBean.class);
   protected static final String TEMPLATE = "Hello, %s!";
 
   public String getGreeting(@NotNull @Pattern(regexp = "\\p{IsAlphabetic}+") final String name) {
+    LOGGER.info("Greeting request from: {}", name);
     return String.format(TEMPLATE, name);
   }
 }
